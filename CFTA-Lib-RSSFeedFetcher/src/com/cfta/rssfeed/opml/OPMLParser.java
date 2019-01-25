8c304f97b65afc04a650eff91d37aedf30e164ab
Lassi Maksimainen <lassi.maksimainen@gmail.com>
2019-01-25 22:58:43 +0100

    Added rudimentary support for OPML 1.1, better parsing for the feeds

--- CFTA-Lib-RSSFeedFetcher/src/com/cfta/rssfeed/opml/OPMLParser.java
+++ CFTA-Lib-RSSFeedFetcher/src/com/cfta/rssfeed/opml/OPMLParser.java
@@ -14,24 +14,28 @@ import org.w3c.dom.NodeList;
 import org.w3c.dom.Node;
 import org.xml.sax.SAXException;
 
+// Parses OPML (Outline Processor Markup Language) Files
 public class OPMLParser {
     private static final String OPML_HEADER = "opml";
     private static final String OPML_VERSION_ATTR = "version";
     private static final String BODY_HEADER = "body";
+    private static final String HEAD_HEADER = "head";
+    private static final String OPML_TITLE = "title";
     private static final String OUTLINE_ELEM = "outline";
 
     private static final String ROOT_FOLDER = "root";
 
+    private static final String EMPTY_STRING = "";
+
+    // Identified OPML version
     private enum OPMLVersion {
         eUnknown,
         eVersion1,
+        eVersion11,
         eVersion2
     }
-    
-    /*private boolean isFolder(Node n) {                
-        return false;
-    }*/
 
+    // Returns attribute value or null if none
     private String getAttributeValueOrNull(String attrName, Node node) {
         if (node.getAttributes() != null && node.getAttributes().getNamedItem(attrName) != null) {
             return node.getAttributes().getNamedItem(attrName).getNodeValue();
@@ -39,11 +43,12 @@ public class OPMLParser {
         return null;
     }
 
+    // Returns attribute value or empty string if none
     private String getAttributeValueOrEmpty(String attrName, Node node) {
         if (node.getAttributes() != null && node.getAttributes().getNamedItem(attrName) != null) {
             return node.getAttributes().getNamedItem(attrName).getNodeValue();
         }
-        return "";
+        return EMPTY_STRING;
     }
 
     // Parses OPML 1.0 doc
@@ -54,13 +59,21 @@ public class OPMLParser {
                 if (typeAttr == null || typeAttr.equalsIgnoreCase("text")) {
                     // is folder
                     String title = getAttributeValueOrEmpty("title", outlineNodes.item(i));
+                    if (title.length() == 0) {
+                        title = getAttributeValueOrEmpty("text", outlineNodes.item(i));
+                    }
                     RSSFeedFolder folder = new RSSFeedFolder(title);
                     parentFolder.folders.add(folder);
                     parseOPML10Doc(outlineNodes.item(i).getChildNodes(), folder);
                 } else if (typeAttr.equalsIgnoreCase("rss") || typeAttr.equalsIgnoreCase("link")) {
                     // is RSS feed
                     String title = getAttributeValueOrEmpty("title", outlineNodes.item(i));
+
+                    if (title.length() == 0) {
+                        title = getAttributeValueOrEmpty("text", outlineNodes.item(i));
+                    }
                     String xmlUrl = getAttributeValueOrEmpty("xmlUrl", outlineNodes.item(i));
+                    if (xmlUrl.length() == 0) { xmlUrl = getAttributeValueOrEmpty("xmlURL", outlineNodes.item(i)); }
                     String htmlUrl = getAttributeValueOrEmpty("htmlUrl", outlineNodes.item(i));
 
                     if (xmlUrl.length() > 0 && xmlUrl.toLowerCase().startsWith("http")) {
@@ -91,6 +104,8 @@ public class OPMLParser {
         OPMLVersion opmlVer = OPMLVersion.eUnknown;
         if (version.trim().equalsIgnoreCase("1.0") || version.trim().equalsIgnoreCase("1")) {
             opmlVer = OPMLVersion.eVersion1;
+        } else if (version.trim().equalsIgnoreCase("1.1")) {
+            opmlVer = OPMLVersion.eVersion11;
         } else if (version.trim().equalsIgnoreCase("2.0") || version.trim().equalsIgnoreCase("2")) {
             opmlVer = OPMLVersion.eVersion2;
         }
@@ -102,10 +117,22 @@ public class OPMLParser {
         RSSFeedFolder folder = null;
         boolean bodyNodeFound = false;
         NodeList nl = doc.getDocumentElement().getChildNodes();
+        String title = "<unknown OPML feed>";
+
         for (int i = 0; i < nl.getLength(); i++) {
-            if (nl.item(i).getNodeName().equalsIgnoreCase(BODY_HEADER)) {
+            if (nl.item(i).getNodeName().equalsIgnoreCase(HEAD_HEADER)) {
+                for (int j = 0; j < nl.item(i).getChildNodes().getLength(); j++) {
+                    if (nl.item(i).getChildNodes().item(j).getNodeName().equalsIgnoreCase(OPML_TITLE)) {
+                        title = nl.item(i).getChildNodes().item(j).getTextContent().trim();
+                        break;
+                    }
+                }
+            } else if (nl.item(i).getNodeName().equalsIgnoreCase(BODY_HEADER)) {
                 if (opmlVer == OPMLVersion.eVersion1) {
-                    folder = new RSSFeedFolder(ROOT_FOLDER);
+                    folder = new RSSFeedFolder(title);
+                    parseOPML10Doc(nl.item(i).getChildNodes(), folder);
+                } else if (opmlVer == OPMLVersion.eVersion11) {
+                    folder = new RSSFeedFolder(title);
                     parseOPML10Doc(nl.item(i).getChildNodes(), folder);
                 } else if (opmlVer == OPMLVersion.eVersion2) {
                     throw new RuntimeException("OPML v2.0 parser not yet implemented!");
