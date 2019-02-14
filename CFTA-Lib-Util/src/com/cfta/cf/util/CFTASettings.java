fe50895d5312f99e906a88edd714717c0f210ab4
Lassi Maksimainen <lassi.maksimainen@gmail.com>
2019-02-14 20:27:43 +0100

    Restructured CFTA-Lib-Util. Removed RSS feed cleaner and added proper structure for unit tests

--- CFTA-Lib-Util/src/com/cfta/cf/util/CFTASettings.java
+++ CFTA-Lib-Util/src/com/cfta/cf/util/CFTASettings.java
@@ -1,143 +1,144 @@
-// CFTA -- Content Fetching & Text Analysis System
-// Lassi Maksimainen, 2019
 package com.cfta.cf.util;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
-// Utility settings for CFTA library
+// Reading settings for CFTA library usage
 public class CFTASettings {
 
-    static private Properties propertiesFile = null;
+  private static Properties propertiesFile = null;
 
-    private static final long DEFAULT_HTTP_CONNECT_TIMEOUT = 2500;
-    private static final long DEFAULT_HTTP_TIMEOUT = 10 * 1000;
-    private static final String DEFAULT_HTTP_USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.69 Safari/537.36";
-    private static final long DEFAULT_RSS_PARSING_TIMEOUT = 5 * 1000;
-    private static final String DEFAULT_DEBUG = "true";
+  private static final long DEFAULT_HTTP_CONNECT_TIMEOUT = 2500;
+  private static final long DEFAULT_HTTP_TIMEOUT = 10 * 1000;
+  private static final String DEFAULT_HTTP_USER_AGENT =
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.69 Safari/537.36";
+  private static final long DEFAULT_RSS_PARSING_TIMEOUT = 5 * 1000;
+  private static final String DEFAULT_DEBUG = "true";
 
-    // Set the proper language profile directory via settings file in order to be able to user language detection
-    private static final String LANG_DETECT_PROF_DIRECTORY = "..\\CFTA-Lib-TextAnalysis\\res\\profiles";
+  // Set the proper language profile directory via settings file in order to be able to user
+  // language detection
+  private static final String LANG_DETECT_PROF_DIRECTORY =
+      "../CFTA-Lib-TextAnalysis/res/profiles";
 
-    // Set the proper stopword list directory for different languages via settings file in order to be able to use stopword filtering
-    private static final String STOPWORD_LIST_DIRECTORY = "";
+  // Set the proper stopword list directory for different languages via settings file in order to be
+  // able to use stopword filtering
+  private static final String STOPWORD_LIST_DIRECTORY = "";
 
-    // Register your own Twitter app to get consumer & access key and secret
-    private static final String OAUTH_CONSUMER_KEY = "";
-    private static final String OAUTH_CONSUMER_SECRET = "";
-    private static final String OAUTH_ACCESS_TOKEN = "";
-    private static final String OAUTH_ACCESS_SECRET = "";
+  // Register your own Twitter app to get consumer & access key and secret
+  private static final String OAUTH_CONSUMER_KEY = "";
+  private static final String OAUTH_CONSUMER_SECRET = "";
+  private static final String OAUTH_ACCESS_TOKEN = "";
+  private static final String OAUTH_ACCESS_SECRET = "";
 
-    // Private constructor, this class should never be instantiated
-    private CFTASettings() {
+  // Private constructor, this class should never be instantiated
+  private CFTASettings() {}
+
+  // Returns value from configuration file
+  private static String getVal(final String valueName) {
+    if (propertiesFile == null) {
+      return null;
     }
 
-    // Returns value from configuration file
-    static private String getVal(String valueName) {
-        if (propertiesFile == null) {
-            return null;
-        }
+    return propertiesFile.getProperty(valueName).trim();
+  }
 
-        return propertiesFile.getProperty(valueName).trim();
+  // Returns value formatted as long number
+  private static long returnLong(final String paramName, long defaultValue) {
+    try {
+      String s;
+      if ((s = getVal(paramName)) != null) {
+        return Long.parseLong(s);
+      } else {
+        return defaultValue;
+      }
+    } catch (Exception ex) {
+      return defaultValue;
     }
+  }
 
-    // Returns value formatted as long number
-    static private long returnLong(String paramName, long defaultValue) {
-        try {
-            String s;
-            if ((s = getVal(paramName)) != null) {
-                return Long.parseLong(s);
-            } else {
-                return defaultValue;
-            }
-        } catch (Exception ex) {
-            return defaultValue;
-        }
+  // Returns value formatted as a string
+  private static String returnString(String paramName, String defaultValue) {
+    try {
+      String s;
+      if ((s = getVal(paramName)) != null) {
+        return s;
+      } else {
+        return defaultValue;
+      }
+    } catch (Exception ex) {
+      return defaultValue;
     }
+  }
 
-    // Returns value formatted as a string
-    static private String returnString(String paramName, String defaultValue) {
-        try {
-            String s;
-            if ((s = getVal(paramName)) != null) {
-                return s;
-            } else {
-                return defaultValue;
-            }
-        } catch (Exception ex) {
-            return defaultValue;
-        }
+  // Returns value formatted as a boolean
+  private static boolean returnBoolean(String paramName, String defaultValue) {
+    try {
+      String s;
+      if ((s = getVal(paramName)) != null) {
+        return s.equalsIgnoreCase("true");
+      } else {
+        return defaultValue.equalsIgnoreCase("true");
+      }
+    } catch (Exception ex) {
+      return defaultValue.equalsIgnoreCase("true");
     }
+  }
 
-    // Returns value formatted as a boolean
-    static private boolean returnBoolean(String paramName, String defaultValue) {
-        try {
-            String s;
-            if ((s = getVal(paramName)) != null) {
-                return s.equalsIgnoreCase("true");
-            } else {
-                return defaultValue.equalsIgnoreCase("true");
-            }
-        } catch (Exception ex) {
-            return defaultValue.equalsIgnoreCase("true");
-        }
+  // Loads settings file -- call this at the application start-up
+  public static void loadSettingsFile(String fileName) {
+    try {
+      propertiesFile = new Properties();
+      propertiesFile.load(new FileInputStream(fileName));
+    } catch (IOException ex) {
+      propertiesFile = null;
+      ex.printStackTrace();
     }
+  }
 
-    // Loads settings file -- call this at the application start-up
-    static public void loadSettingsFile(String fileName) {
-        try {
-            propertiesFile = new Properties();
-            propertiesFile.load(new FileInputStream(fileName));
-        } catch (IOException ex) {
-            propertiesFile = null;
-            ex.printStackTrace();
-        }
-    }
+  /* Getters to Return the various configuration values from settings file or from defaults */
 
-    /* Getters to Return the various configuration values from settings file or from defaults */
+  public static long getHTTPConnectTimeout() {
+    return returnLong("httpConnectTimeout", DEFAULT_HTTP_CONNECT_TIMEOUT);
+  }
 
-    static public long getHTTPConnectTimeout() {
-        return returnLong("httpConnectTimeout", DEFAULT_HTTP_CONNECT_TIMEOUT);
-    }
+  public static long getHTTPTimeout() {
+    return returnLong("httpTimeout", DEFAULT_HTTP_TIMEOUT);
+  }
 
-    static public long getHTTPTimeout() {
-        return returnLong("httpTimeout", DEFAULT_HTTP_TIMEOUT);
-    }
+  public static String getHTTPUserAgent() {
+    return returnString("httpUserAgent", DEFAULT_HTTP_USER_AGENT);
+  }
 
-    static public String getHTTPUserAgent() {
-        return returnString("httpUserAgent", DEFAULT_HTTP_USER_AGENT);
-    }
+  public static long getRSSFeedParseTimeout() {
+    return returnLong("rssFeedParseTimeout", DEFAULT_RSS_PARSING_TIMEOUT);
+  }
 
-    static public long getRSSFeedParseTimeout() {
-        return returnLong("rssFeedParseTimeout", DEFAULT_RSS_PARSING_TIMEOUT);
-    }
+  public static boolean getDebug() {
+    return returnBoolean("debug", DEFAULT_DEBUG);
+  }
 
-    static public boolean getDebug() {
-        return returnBoolean("debug", DEFAULT_DEBUG);
-    }
+  public static String getTwitterOAuthConsumerKey() {
+    return returnString("twitterOauthConsumerKey", OAUTH_CONSUMER_KEY);
+  }
 
-    static public String getTwitterOAuthConsumerKey() {
-        return returnString("twitterOauthConsumerKey", OAUTH_CONSUMER_KEY);
-    }
+  public static String getTwitterOAuthConsumerSecret() {
+    return returnString("twitterOauthConsumerSecret", OAUTH_CONSUMER_SECRET);
+  }
 
-    static public String getTwitterOAuthConsumerSecret() {
-        return returnString("twitterOauthConsumerSecret", OAUTH_CONSUMER_SECRET);
-    }
+  public static String getTwitterAccessToken() {
+    return returnString("twitterOauthAccessToken", OAUTH_ACCESS_TOKEN);
+  }
 
-    static public String getTwitterAccessToken() {
-        return returnString("twitterOauthAccessToken", OAUTH_ACCESS_TOKEN);
-    }
+  public static String getTwitterAccessSecret() {
+    return returnString("twitterOauthAccessSecret", OAUTH_ACCESS_SECRET);
+  }
 
-    static public String getTwitterAccessSecret() {
-        return returnString("twitterOauthAccessSecret", OAUTH_ACCESS_SECRET);
-    }
+  public static String getLangDetectProfileDir() {
+    return returnString("langDetectProfileDir", LANG_DETECT_PROF_DIRECTORY);
+  }
 
-    static public String getLangDetectProfileDir() {
-        return returnString("langDetectProfileDir", LANG_DETECT_PROF_DIRECTORY);
-    }
-
-    static public String getStopwordListDir() {
-        return returnString("stopwordListDir", STOPWORD_LIST_DIRECTORY);
-    }
+  public static String getStopwordListDir() {
+    return returnString("stopwordListDir", STOPWORD_LIST_DIRECTORY);
+  }
 }
--- CFTA-Lib-Util/src/com/cfta/log/CFTALog.java
+++ CFTA-Lib-Util/src/com/cfta/log/CFTALog.java
@@ -1,16 +1,14 @@
-// CFTA -- Content Fetching & Text Analysis System
-// Lassi Maksimainen, 2019
 package com.cfta.log;
 
 import com.cfta.cf.util.CFTASettings;
 
-// Logger utility class
-public class CFTALog {
+// Logger utility class, should move to use only standard Apache logging
+@Deprecated public class CFTALog {
 
-    // Stands for "log a line"
-    static public void LL(String output) {
-        if (CFTASettings.getDebug()) {
-            System.out.println(output);
-        }
+  // Stands for "log a line"
+  public static void LL(String output) {
+    if (CFTASettings.getDebug()) {
+      System.out.println(output);
     }
+  }
 }
