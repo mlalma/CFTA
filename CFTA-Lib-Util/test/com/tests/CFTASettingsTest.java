dc2d0f185214349bbe6b6adcbf2e364d95cf7451
Lassi Maksimainen <lassi.maksimainen@gmail.com>
2019-02-16 19:24:30 +0100

    Added unit test for CFTASettings

--- CFTA-Lib-Util/test/com/tests/CFTASettingsTest.java
+++ CFTA-Lib-Util/test/com/tests/CFTASettingsTest.java
@@ -1,12 +1,28 @@
 package com.tests;
 
+import com.cfta.cf.util.CFTASettings;
 import org.junit.Assert;
 import org.junit.Test;
+import com.tests.TestUtil;
 
 public class CFTASettingsTest {
 
   @Test
   public void testSettings() {
-    Assert.assertTrue(true);
+    String fileName = TestUtil.getTestResourceFile("test.properties").getAbsolutePath();
+
+    CFTASettings.loadSettingsFile(fileName);
+
+    Assert.assertTrue(CFTASettings.getHTTPConnectTimeout() == 100);
+    Assert.assertTrue(CFTASettings.getHTTPTimeout() == 101);
+    Assert.assertTrue(CFTASettings.getHTTPUserAgent().equals("TestUserAgent"));
+    Assert.assertTrue(CFTASettings.getRSSFeedParseTimeout() == 102);
+    Assert.assertTrue(CFTASettings.getDebug());
+    Assert.assertTrue(CFTASettings.getTwitterOAuthConsumerKey().equals("OAuthCKey"));
+    Assert.assertTrue(CFTASettings.getTwitterOAuthConsumerSecret().equals("OAuthConsumerSecret"));
+    Assert.assertTrue(CFTASettings.getTwitterAccessSecret().equals("OAuthAccessSecret"));
+    Assert.assertTrue(CFTASettings.getTwitterAccessToken().equals("OAuthAccessToken"));
+    Assert.assertTrue(CFTASettings.getLangDetectProfileDir().equals("/lang/profile/dir"));
+    Assert.assertTrue(CFTASettings.getStopwordListDir().equals("/stopword/list/dir"));
   }
 }
